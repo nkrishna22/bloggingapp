8155c597619625df7e39d85226c5c2c2740f0b6c
nkrishna22 <ravikrishnanaidu@gmail.com>
2024-03-10 23:33:42 +0530

    spring boot app recreated

--- bloggingapp/src/main/java/ravi/learning/bloggingapp/BloggingappApplication.java
+++ bloggingapp/src/main/java/ravi/learning/bloggingapp/BloggingappApplication.java
@@ -1,11 +1,13 @@
-package ravi.learning;
+package ravi.learning.bloggingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App {
+public class BloggingappApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(BloggingappApplication.class, args);
     }
+
 }
---
+++ bloggingapp/src/test/java/ravi/learning/bloggingapp/BloggingappApplicationTests.java
@@ -0,0 +1,13 @@
+package ravi.learning.bloggingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
