85a7decf392cd7c8dbe0c0035a49d6c68b7659ab
coderravikrishna <coderravikrishna@gmail.com>
2024-05-06 22:51:53 +0530

    post API to show all comments

--- bloggingapp/src/main/java/ravi/learning/bloggingapp/dto/PostDto.java
+++ bloggingapp/src/main/java/ravi/learning/bloggingapp/dto/PostDto.java
@@ -1,6 +1,9 @@
 package ravi.learning.bloggingapp.dto;
 
 import lombok.Data;
+import ravi.learning.bloggingapp.model.Comment;
+
+import java.util.Set;
 
 @Data
 public class PostDto {
@@ -8,4 +11,5 @@ public class PostDto {
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 }
