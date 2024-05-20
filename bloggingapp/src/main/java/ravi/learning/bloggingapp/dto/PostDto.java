7bd4392c2d414cd007eaba47ca581a72519aceeb
coderravikrishna <coderravikrishna@gmail.com>
2024-05-20 21:05:42 +0530

    Spring boot validation added

--- bloggingapp/src/main/java/ravi/learning/bloggingapp/dto/PostDto.java
+++ bloggingapp/src/main/java/ravi/learning/bloggingapp/dto/PostDto.java
@@ -1,5 +1,7 @@
 package ravi.learning.bloggingapp.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import ravi.learning.bloggingapp.model.Comment;
 
@@ -8,7 +10,17 @@ import java.util.Set;
 @Data
 public class PostDto {
     private Long id;
+
+//    title should not be empty or null
+//    title should have at least 2 characters
+    @NotEmpty
+    @Size(min = 2, message = "Post title should have at least 2 characters")
     private String title;
+
+//    Post description should not be empty or null
+//    Post description should at least have 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 chracters")
     private String description;
     private String content;
     private Set<CommentDto> comments;
