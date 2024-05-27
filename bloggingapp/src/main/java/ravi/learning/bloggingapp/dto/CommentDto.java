a746ce89a9390076eab2f91c70c00a0a410b9ad5
coderravikrishna <coderravikrishna@gmail.com>
2024-05-28 00:11:43 +0530

    Comment validation updated

--- bloggingapp/src/main/java/ravi/learning/bloggingapp/dto/CommentDto.java
+++ bloggingapp/src/main/java/ravi/learning/bloggingapp/dto/CommentDto.java
@@ -1,12 +1,27 @@
 package ravi.learning.bloggingapp.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private Long id;
+//    Name should not be null or empty
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+//    Email should not be null or empty
+//    email field validation
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+//    comment body should not be null or empty
+//    comment body must be minimum 10 characters
+    @NotEmpty(message = "Comment body should not be null or empty")
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
     private String body;
 
 }
