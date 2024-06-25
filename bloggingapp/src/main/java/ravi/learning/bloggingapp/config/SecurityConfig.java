01ee8cd29ffde0f814571a8751e80b8914deced0
nkrishna22 <ravikrishnanaidu@gmail.com>
2024-06-25 14:13:54 +0530

    Updated in-memory authentication

--- bloggingapp/src/main/java/ravi/learning/bloggingapp/config/SecurityConfig.java
+++ bloggingapp/src/main/java/ravi/learning/bloggingapp/config/SecurityConfig.java
@@ -4,15 +4,41 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails ramesh = User.builder().username("ramesh")
+                .password(passwordEncoder().encode("testPassword123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder().username("admin")
+                .password(passwordEncoder().encode("testPassword123"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(ramesh, admin);
+    }
+
 }
